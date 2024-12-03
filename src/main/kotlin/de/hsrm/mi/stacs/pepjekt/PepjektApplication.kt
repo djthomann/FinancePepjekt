c56739bf20e050d381ec7421ae97206c779328f6
Artur Konkel <Artur.Konkel@student.hs-rm.de>
2024-12-03 12:15:52 +0100

    Initial commit

---
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/PepjektApplication.kt
@@ -0,0 +1,11 @@
+package de.hsrm.mi.stacs.pepjekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PepjektApplication
+
+fun main(args: Array<String>) {
+	runApplication<PepjektApplication>(*args)
+}
---
+++ src/test/kotlin/de/hsrm/mi/stacs/pepjekt/PepjektApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.hsrm.mi.stacs.pepjekt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PepjektApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
