7454fabd8bf50a6c1425aa23f28ab480e53ebc61
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2024-12-12 14:57:12 +0100

    feature: add IBankAccountService and Test

--- src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IBankAccountService.kt
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IBankAccountService.kt
@@ -1,12 +1,13 @@
 package de.hsrm.mi.stacs.pepjekt.services
 
+import reactor.core.publisher.Mono
 import java.math.BigDecimal
 
 interface IBankAccountService {
 
-    fun getBalance(bankAccountId: Long): BigDecimal
+    fun getBalance(bankAccountId: Long): Mono<BigDecimal>
 
-    fun deposit(bankAccountId: Long, amount: BigDecimal): Void
+    fun deposit(bankAccountId: Long, amount: BigDecimal)
 
-    fun withdraw(bankAccountId: Long, amount: BigDecimal): Void
+    fun withdraw(bankAccountId: Long, amount: BigDecimal)
 }
