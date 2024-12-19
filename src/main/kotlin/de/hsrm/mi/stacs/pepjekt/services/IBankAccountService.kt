f54d1c0b91ed19dbd88fbf01238abca83039885b
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2024-12-19 10:02:05 +0100

    feature: add javadoc to service interfaces

--- src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IBankAccountService.kt
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IBankAccountService.kt
@@ -3,6 +3,12 @@ package de.hsrm.mi.stacs.pepjekt.services
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
 
+/**
+ * Interface for managing bank account operations.
+ *
+ * Provides methods for retrieving balances, depositing funds, and withdrawing funds
+ * from bank accounts in a reactive programming paradigm.
+ */
 interface IBankAccountService {
 
     fun getBalance(bankAccountId: Long): Mono<BigDecimal>
--- src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IInvestmentAccountService.kt
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IInvestmentAccountService.kt
@@ -4,6 +4,12 @@ import de.hsrm.mi.stacs.pepjekt.entities.InvestmentAccount
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
 
+/**
+ * Interface for managing investment account operations.
+ *
+ * Provides methods for buying and selling stocks in an investment account, as well as retrieving
+ * the portfolio details of an investment account.
+ */
 interface IInvestmentAccountService {
 
     fun buyStock(investmentAccountId: Long, stockSymbol: String, volume: BigDecimal): Mono<InvestmentAccount>
