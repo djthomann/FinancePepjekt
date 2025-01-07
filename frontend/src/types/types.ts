0ae37fe083d8575555ed0a2a9462c497829ff0a4
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-07 14:29:24 +0100

    feature: add InvestmentAccount, PortfolioEntry, BankAccount and User to types.ts

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -21,3 +21,36 @@ export interface Stock {
 export enum Currency {
   USD = 'USD'
 }
+
+export interface InvestmentAccount {
+  id?: number,
+  bankAccountId: number,
+  portfolio: PortfolioEntry[],
+  userId: number,
+  bankAccount: BankAccount,
+  user: User
+}
+
+export interface PortfolioEntry {
+  id: number,
+  investmentAccountId: number,
+  stockSymbol: String,
+  quantity: number,
+  stock: Stock,
+  currentValue: number,
+  amount: number,
+  change: number,
+  changePercentage: number
+}
+
+export interface BankAccount {
+  id: number,
+  currency: Currency,
+  balance: number
+}
+
+export interface User {
+  id: number,
+  name: String,
+  mail: String
+}
