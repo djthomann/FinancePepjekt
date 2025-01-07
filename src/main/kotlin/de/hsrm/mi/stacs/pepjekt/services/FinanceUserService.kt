02e09b6e5ca42e765189f5068d95638ccae1208d
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-07 19:09:16 +0100

    feature: add finance user as repository and service

---
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/FinanceUserService.kt
@@ -0,0 +1,22 @@
+package de.hsrm.mi.stacs.pepjekt.services
+
+import de.hsrm.mi.stacs.pepjekt.entities.User
+import de.hsrm.mi.stacs.pepjekt.repositories.IFinanceUserRepository
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+/**
+ * Service for retrieving the users.
+ *
+ * This service provides methods for retrieving user details.
+ */
+@Service
+class FinanceUserService(
+    val financeUserRepository: IFinanceUserRepository
+) : IFinanceUserService {
+
+    override fun getUserId(id: Long): Mono<User> {
+        return financeUserRepository.findById(id);
+    }
+
+}
---
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/services/IFinanceUserService.kt
@@ -0,0 +1,15 @@
+package de.hsrm.mi.stacs.pepjekt.services
+
+import de.hsrm.mi.stacs.pepjekt.entities.User
+import reactor.core.publisher.Mono
+
+/**
+ * Interface for managing the user.
+ *
+ * Provides methods for retrieving user details.
+ */
+interface IFinanceUserService {
+
+    fun getUserId(id: Long): Mono<User>
+
+}
