ac1644cea95f558b36f04ea22a032e41904b9dcf
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-07 14:46:38 +0100

    feature: stock overview fully (restwise) fetched

--- frontend/src/components/StockOverview.vue
+++ frontend/src/components/StockOverview.vue
@@ -1,50 +1,61 @@
 <template>
   <div class="invest-depot">
-      <div>
-            <h1>Wertpapiere</h1>
-            <div id="searchField">
-                  <input v-model="searchField" placeholder="Symbol/Name" />
-                  <button class="details-button" @click="resetSearch">Reset</button>
-                  <button class="details-button" @click="searchContent">Search</button>
-            </div>
+    <div>
+      <h1>Wertpapiere</h1>
+      <div id="searchField">
+        <input v-model="searchField" placeholder="Symbol/Name"/>
+        <button class="details-button" @click="resetSearch">Reset</button>
+        <button class="details-button" @click="searchContent">Search</button>
       </div>
+    </div>
 
-      <div>
-            <table>
-                  <thead>
-                        <tr>
-                              <th>Name</th>
-                              <th>ISIN</th>
-                              <th>Aktueller Wert</th>
-                              <th>Gewinn/Verlust</th>
-                        </tr>
-                  </thead>
-                  <tbody>
-                        <tr v-for="position in positions" :key="position.id" @click="navigateToStockDetail(position.symbol)">
-                              <td>{{ position.name }}</td>
-                              <td>{{ position.symbol }}</td>
-                              <td>{{ position.currentValue }} €</td>
-                              <td :class="{ 'positive': position.change >= 0, 'negative': position.change < 0 }">
-                                    {{ position.change }} € ({{ position.changePercentage }}%)
-                              </td>
-                        </tr>
-                  </tbody>
-            </table>
-      </div>
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>FIGI</th>
+          <th>Aktueller Wert</th>
+          <th>Gewinn/Verlust</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="stock in stocks" :key="stock.id" @click="navigateToStockDetail(stock.symbol)">
+          <td>{{ stock.description }}</td>
+          <td>{{ stock.symbol }}</td>
+          <td>{{ stock.figi }}</td>
+          <td>{{ stock.currentValue }} €</td>    <!--reactive value-->
+          <td :class="{ 'positive': stock.change >= 0, 'negative': stock.change < 0 }">
+            {{ stock.change }} € ({{ stock.changePercentage }}%)
+          </td>    <!--reactive value-->
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import {onMounted, ref} from 'vue';
 import {useRouter} from "vue-router";
+import type {Stock} from "@/types/types.ts";
 
 const router = useRouter()
 const searchField = ref('')
-const positions = ref([
-  { id: 1, name: 'Apple', symbol: "2", currentValue: 1450.90, change: 33.39, changePercentage: 13.6 },
-  { id: 2, name: 'Tesla', symbol: "5", currentValue: 3565.35, change: 120.75, changePercentage: 20.1 },
-  { id: 3, name: 'Amazon', symbol: "10", currentValue: 6169.52, change: -45.50, changePercentage: -5.2 },
-])
+const stocks = ref<Stock[]>([])
+
+onMounted(async () => {
+  try {
+    const response = await fetch("/api/stocks")
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    stocks.value = await response.json() as Stock[]
+  } catch (e) {
+    console.error(e)
+  }
+})
 
 function resetSearch() {
   searchField.value = ''
