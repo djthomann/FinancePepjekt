4b6cc0b895b7b975a01e937efce8823de0ba9371
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-19 13:26:30 +0100

    fix: fix InvestmentAccountServiceTest test

--- src/test/kotlin/de/hsrm/mi/stacs/pepjekt/services/InvestmentAccountServiceTest.kt
+++ src/test/kotlin/de/hsrm/mi/stacs/pepjekt/services/InvestmentAccountServiceTest.kt
@@ -1,19 +1,20 @@
 package de.hsrm.mi.stacs.pepjekt.services
 
-import de.hsrm.mi.stacs.pepjekt.entities.BankAccount
-import de.hsrm.mi.stacs.pepjekt.entities.Currency
-import de.hsrm.mi.stacs.pepjekt.entities.InvestmentAccount
-import de.hsrm.mi.stacs.pepjekt.entities.Stock
+import de.hsrm.mi.stacs.pepjekt.entities.*
+import de.hsrm.mi.stacs.pepjekt.entities.dtos.*
 import de.hsrm.mi.stacs.pepjekt.handler.FinnhubHandler
 import de.hsrm.mi.stacs.pepjekt.repositories.*
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.mockito.ArgumentMatchers.any
 import org.springframework.transaction.reactive.TransactionalOperator
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
 import org.assertj.core.api.Assertions.assertThat
 import org.mockito.Mockito.*
+import java.time.LocalDateTime
+import org.junit.jupiter.api.Assertions.*
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
 
 /**
  * Unit tests for the [InvestmentAccountService] class.
@@ -35,6 +36,10 @@ class InvestmentAccountServiceTest {
     private lateinit var bankAccount: BankAccount
     private lateinit var investmentAccount: InvestmentAccount
     private lateinit var stock: Stock
+    private lateinit var stockQuote: StockQuote
+    private lateinit var portfolioEntry: PortfolioEntry
+    private lateinit var investmentAccountDTO: InvestmentAccountDTO
+    private lateinit var owner: Owner
 
     /**
      * Initializes the test environment by setting up mock data and dependencies.
@@ -47,15 +52,41 @@ class InvestmentAccountServiceTest {
             balance = BigDecimal(150),
             currency = Currency.USD
         )
+        owner = Owner(
+            id = 1L,
+            name = "Hans",
+            mail = "hans@example.com",
+        )
         stock = Stock(stockSymbol, "Apple Inc.", "BBG000B9XRY4", "figi", Currency.USD)
+        stockQuote = StockQuote(
+            currentPrice = BigDecimal(100),
+            timeStamp = LocalDateTime.now().minusDays(1),
+            stockSymbol = stock.symbol,
+            id = 1L,
+            change = 10.0F,
+            percentChange = 10.0F,
+            highPriceOfTheDay = BigDecimal(120),
+            lowPriceOfTheDay = BigDecimal(70),
+            openPriceOfTheDay = BigDecimal(100),
+            previousClosePrice = BigDecimal(100)
+        )
         investmentAccount = InvestmentAccount(bankAccountId = bankAccount.id, id = 1L, ownerId = 1L)
+        portfolioEntry = PortfolioEntry(investmentAccountId = investmentAccount.id!!, stockSymbol = stock.symbol, id = 1L, quantity = 10.0)
 
         `when`(investmentAccountRepository.findByOwnerId(1L)).thenReturn(Mono.just(investmentAccount))
         `when`(investmentAccountRepository.save(any())).thenReturn(Mono.just(investmentAccount))
-        `when`(investmentAccountRepository.findByBankAccountId(1L)).thenReturn(Mono.just(investmentAccount))
-        `when`(investmentAccountRepository.findById(1L)).thenReturn(Mono.just(investmentAccount))
+        `when`(investmentAccountRepository.findByBankAccountId(bankAccount.id!!)).thenReturn(Mono.just(investmentAccount))
+        `when`(investmentAccountRepository.findById(investmentAccount.id!!)).thenReturn(Mono.just(investmentAccount))
         `when`(stockRepository.findBySymbol(stockSymbol)).thenReturn(Mono.just(stock))
         `when`(stockRepository.findById(stockSymbol)).thenReturn(Mono.just(stock))
+        `when`(bankAccountRepository.findById(bankAccount.id!!)).thenReturn(Mono.just(bankAccount))
+        `when`(portfolioEntryRepository.findByInvestmentAccountIdAndStockSymbol(investmentAccount.id!!, stockSymbol)).thenReturn(Mono.just(portfolioEntry))
+        `when`(portfolioEntryRepository.findByInvestmentAccountId(investmentAccount.id!!)).thenReturn(Flux.just(portfolioEntry))
+        `when`(portfolioEntryRepository.save(any())).thenAnswer { Mono.just(portfolioEntry) }
+        `when`(bankAccountRepository.save(any())).thenAnswer { Mono.just(bankAccount) }
+        `when`(finnhubHandler.fetchStockQuote(stockSymbol)).thenReturn(Mono.just(stockQuote))
+        `when`(ownerRepository.findById(owner.id)).thenReturn(Mono.just(owner))
+        `when`(stockService.getLatestQuoteBySymbol(stockSymbol)).thenReturn(Mono.just(stockQuote))
 
         investmentAccountService = InvestmentAccountService(operator, investmentAccountRepository, portfolioEntryRepository,
             bankAccountRepository, ownerRepository, stockRepository, finnhubHandler, stockService)
@@ -63,38 +94,52 @@ class InvestmentAccountServiceTest {
 
     /**
      * Tests the [InvestmentAccountService.sellStock] method to ensure that selling stock updates the portfolio correctly.
+     *
+     * TODO add once implemented
      */
-    /*@Test
+    @Test
     fun `test sell stock`() {
-        investmentAccountService.sellStock(1L, stock.symbol, BigDecimal(10))
-            .doOnNext {
-                assertThat(it).isNotNull
-                it.portfolio[stock]?.let { it1 -> assertThat(it1.compareTo(160)) }
-                verify(investmentAccountRepository).save(any())
-            }
+        val result = investmentAccountService.sellStock(investmentAccount.id!!, stock.symbol, BigDecimal(10)).block()
+
+        assertNotNull(result)
+        assertEquals(investmentAccount.id!!, result!!.id)
+        verify(bankAccountRepository).findById(1L)
+        verify(portfolioEntryRepository).findByInvestmentAccountIdAndStockSymbol(investmentAccount.id!!, stock.symbol)
+        verify(portfolioEntryRepository).save(any())
+        verify(bankAccountRepository).save(any())
     }
 
     /**
      * Tests the [InvestmentAccountService.buyStock] method to ensure that buying stock updates the portfolio correctly.
+     *
+     * TODO add once implemented
      */
     @Test
     fun `test buy stock`() {
-        investmentAccountService.buyStock(1L, stock.symbol, BigDecimal(10))
-            .doOnNext {
-                assertThat(it).isNotNull
-                it.portfolio[stock]?.let { it1 -> assertThat(it1.compareTo(140)) }
-                verify(investmentAccountRepository).save(any())
-            }
+        val result = investmentAccountService.buyStock(investmentAccount.id!!, stock.symbol, BigDecimal(10)).block()
+
+        assertNotNull(result)
+        assertEquals(investmentAccount.id!!, result!!.id)
+        verify(bankAccountRepository).findById(1L)
+        verify(portfolioEntryRepository).findByInvestmentAccountIdAndStockSymbol(investmentAccount.id!!, stock.symbol)
+        verify(portfolioEntryRepository).save(any())
+        verify(bankAccountRepository).save(any())
     }
 
     /**
      * Tests the [InvestmentAccountService.getInvestmentAccountPortfolio] method to ensure that it retrieves the correct investment account.
      */
     @Test
-    fun `test get investment account`() {
-        investmentAccountService.getInvestmentAccountPortfolio(1L)
-            .doOnNext { account -> assertThat(investmentAccount == account) }
+    fun `test getInvestmentAccountPortfolio returns the correct DTO`() {
+        val result = investmentAccountService.getInvestmentAccountPortfolio(1L).block()
+
+        assertNotNull(result)
+        assertEquals(1L, result!!.id)
+        assertEquals(1L, result.bankAccountId)
+        assertEquals(1, result.portfolio.size)
+        assertEquals(OwnerDTO.mapToDto(owner), result.owner)
+        //assertEquals(StockDTO.mapToDto(stock, stockQuote), result.portfolio[0].stock)           // TODO wie vergleicht man Big Decimal
     }
 
-     */
+
 }
