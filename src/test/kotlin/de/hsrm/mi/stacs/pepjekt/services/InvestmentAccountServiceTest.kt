a3ce17990ebab6bc8f90e6440effb91b0bd27775
Artur Konkel <Artur.Konkel@student.hs-rm.de>
2025-01-24 11:22:20 +0100

    Refactor InvestmentAccountServiceTestt

--- src/test/kotlin/de/hsrm/mi/stacs/pepjekt/services/InvestmentAccountServiceTest.kt
+++ src/test/kotlin/de/hsrm/mi/stacs/pepjekt/services/InvestmentAccountServiceTest.kt
@@ -1,21 +1,18 @@
 package de.hsrm.mi.stacs.pepjekt.services
 
 import de.hsrm.mi.stacs.pepjekt.entities.*
-import de.hsrm.mi.stacs.pepjekt.entities.dtos.*
 import de.hsrm.mi.stacs.pepjekt.handler.FinnhubHandler
 import de.hsrm.mi.stacs.pepjekt.repositories.*
 import org.junit.jupiter.api.*
 import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.*
 import org.springframework.transaction.reactive.TransactionalOperator
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
-import org.assertj.core.api.Assertions.assertThat
-import org.mockito.Mockito.*
+import java.math.RoundingMode
 import java.time.LocalDateTime
 import org.junit.jupiter.api.Assertions.*
 import reactor.core.publisher.Flux
-import reactor.test.StepVerifier
-import java.math.RoundingMode
 
 /**
  * Unit tests for the [InvestmentAccountService] class.
@@ -24,6 +21,7 @@ import java.math.RoundingMode
  * for the repository and transaction operator.
  */
 class InvestmentAccountServiceTest {
+
     private val investmentAccountRepository: IInvestmentAccountRepository =
         mock(IInvestmentAccountRepository::class.java)
     private val portfolioEntryRepository: IPortfolioEntryRepository = mock(IPortfolioEntryRepository::class.java)
@@ -42,7 +40,6 @@ class InvestmentAccountServiceTest {
     private lateinit var stock: Stock
     private lateinit var stockQuote: StockQuote
     private lateinit var portfolioEntry: PortfolioEntry
-    private lateinit var investmentAccountDTO: InvestmentAccountDTO
     private lateinit var owner: Owner
 
     /**
@@ -50,63 +47,15 @@ class InvestmentAccountServiceTest {
      */
     @BeforeEach
     fun setUp() {
-        val stockSymbol = "AAPL"
-        bankAccount = BankAccount(
-            id = 1L,
-            balance = BigDecimal(150),
-            currency = Currency.USD
-        )
-        owner = Owner(
-            id = 1L,
-            name = "Hans",
-            mail = "hans@example.com",
-        )
-        stock = Stock(stockSymbol, "Apple Inc.", "BBG000B9XRY4", "figi", Currency.USD)
-        stockQuote = StockQuote(
-            currentPrice = BigDecimal(100),
-            timeStamp = LocalDateTime.now().minusDays(1),
-            stockSymbol = stock.symbol,
-            id = 1L,
-            change = 10.0F,
-            percentChange = 10.0F,
-            highPriceOfTheDay = BigDecimal(120),
-            lowPriceOfTheDay = BigDecimal(70),
-            openPriceOfTheDay = BigDecimal(100),
-            previousClosePrice = BigDecimal(100)
-        )
-        investmentAccount = InvestmentAccount(bankAccountId = bankAccount.id, id = 1L, ownerId = 1L)
-        portfolioEntry = PortfolioEntry(
-            investmentAccountId = investmentAccount.id!!, stockSymbol = stock.symbol, id
-            = 1L, quantity = 10.0, totalInvestAmount = 1000.0.toBigDecimal()
-        )
+        bankAccount = createBankAccount()
+        owner = createOwner()
+        stock = createStock("AAPL")
+        stockQuote = createStockQuote("AAPL", BigDecimal(100))
+        investmentAccount = createInvestmentAccount()
+        portfolioEntry = createPortfolioEntry()
 
-        `when`(investmentAccountRepository.findByOwnerId(1L)).thenReturn(Mono.just(investmentAccount))
-        `when`(investmentAccountRepository.save(any())).thenReturn(Mono.just(investmentAccount))
-        `when`(investmentAccountRepository.findByBankAccountId(bankAccount.id!!)).thenReturn(Mono.just(investmentAccount))
-        `when`(investmentAccountRepository.findById(investmentAccount.id!!)).thenReturn(Mono.just(investmentAccount))
-        `when`(stockRepository.findBySymbol(stockSymbol)).thenReturn(Mono.just(stock))
-        `when`(stockRepository.findById(stockSymbol)).thenReturn(Mono.just(stock))
-        `when`(bankAccountRepository.findById(bankAccount.id!!)).thenReturn(Mono.just(bankAccount))
-        `when`(
-            portfolioEntryRepository.findByInvestmentAccountIdAndStockSymbol(
-                investmentAccount.id!!,
-                stockSymbol
-            )
-        ).thenReturn(Mono.just(portfolioEntry))
-        `when`(portfolioEntryRepository.findByInvestmentAccountId(investmentAccount.id!!)).thenReturn(
-            Flux.just(
-                portfolioEntry
-            )
-        )
-        `when`(portfolioEntryRepository.save(any())).thenAnswer { Mono.just(portfolioEntry) }
-        `when`(bankAccountRepository.save(any())).thenAnswer { Mono.just(bankAccount) }
-        `when`(finnhubHandler.fetchStockQuote(stockSymbol)).thenReturn(Mono.just(stockQuote))
-        `when`(ownerRepository.findById(owner.id)).thenReturn(Mono.just(owner))
-        `when`(stockService.getLatestQuoteBySymbol(stockSymbol)).thenReturn(Mono.just(stockQuote))
-
-        `when`(operator.transactional(any(Mono::class.java))).thenAnswer { invocation ->
-            invocation.getArgument<Mono<PortfolioEntry>>(0)
-        }
+        mockRepositories()
+        mockTransactionalOperator()
 
         investmentAccountService = InvestmentAccountService(
             operator,
@@ -121,6 +70,68 @@ class InvestmentAccountServiceTest {
         )
     }
 
+    private fun createBankAccount() = BankAccount(
+        id = 1L, balance = BigDecimal(150), currency = Currency.USD
+    )
+
+    private fun createOwner() = Owner(
+        id = 1L, name = "Hans", mail = "hans@example.com"
+    )
+
+    private fun createStock(symbol: String) = Stock(symbol, "Apple Inc.", "BBG000B9XRY4", "figi", Currency.USD)
+
+    private fun createStockQuote(stockSymbol: String, price: BigDecimal) = StockQuote(
+        currentPrice = price,
+        timeStamp = LocalDateTime.now().minusDays(1),
+        stockSymbol = stockSymbol,
+        id = 1L,
+        change = 10.0F,
+        percentChange = 10.0F,
+        highPriceOfTheDay = BigDecimal(120),
+        lowPriceOfTheDay = BigDecimal(70),
+        openPriceOfTheDay = BigDecimal(100),
+        previousClosePrice = BigDecimal(100)
+    )
+
+    private fun createInvestmentAccount() = InvestmentAccount(bankAccountId = bankAccount.id, id = 1L, ownerId = 1L)
+
+    private fun createPortfolioEntry() = PortfolioEntry(
+        investmentAccountId = investmentAccount.id!!,
+        stockSymbol = stock.symbol,
+        id = 1L,
+        quantity = 10.0,
+        totalInvestAmount = 1000.0.toBigDecimal()
+    )
+
+    private fun mockRepositories() {
+        `when`(investmentAccountRepository.findByOwnerId(1L)).thenReturn(Mono.just(investmentAccount))
+        `when`(investmentAccountRepository.save(any())).thenReturn(Mono.just(investmentAccount))
+        `when`(investmentAccountRepository.findByBankAccountId(bankAccount.id!!)).thenReturn(Mono.just(investmentAccount))
+        `when`(investmentAccountRepository.findById(investmentAccount.id!!)).thenReturn(Mono.just(investmentAccount))
+        `when`(stockRepository.findBySymbol("AAPL")).thenReturn(Mono.just(stock))
+        `when`(stockRepository.findById("AAPL")).thenReturn(Mono.just(stock))
+        `when`(bankAccountRepository.findById(bankAccount.id!!)).thenReturn(Mono.just(bankAccount))
+        `when`(
+            portfolioEntryRepository.findByInvestmentAccountIdAndStockSymbol(
+                investmentAccount.id!!, "AAPL"
+            )
+        ).thenReturn(Mono.just(portfolioEntry))
+        `when`(portfolioEntryRepository.findByInvestmentAccountId(investmentAccount.id!!)).thenReturn(
+            Flux.just(
+                portfolioEntry
+            )
+        )
+        `when`(portfolioEntryRepository.save(any())).thenReturn(Mono.just(portfolioEntry))
+        `when`(finnhubHandler.fetchStockQuote("AAPL")).thenReturn(Mono.just(stockQuote))
+        `when`(ownerRepository.findById(owner.id)).thenReturn(Mono.just(owner))
+        `when`(stockService.getLatestQuoteBySymbol("AAPL")).thenReturn(Mono.just(stockQuote))
+    }
+
+    private fun mockTransactionalOperator() {
+        `when`(operator.transactional(any(Mono::class.java))).thenAnswer { invocation ->
+            invocation.getArgument<Mono<PortfolioEntry>>(0)
+        }
+    }
 
     @Test
     fun `test buyStock successfully updates portfolio and deducts balance`() {
@@ -130,43 +141,27 @@ class InvestmentAccountServiceTest {
         val currentStockPrice = BigDecimal(100)
         val calculatedVolume = purchaseAmount.divide(currentStockPrice, RoundingMode.HALF_UP)
 
-        val investmentAccount = InvestmentAccount(id = investmentAccountId, bankAccountId = 1L)
-        val bankAccount = BankAccount(id = 1L, currency = Currency.USD, balance = BigDecimal(500))
-
-        val stockQuote = StockQuote(
-            id = 1L, currentPrice = currentStockPrice, change = 0f, percentChange = 0f,
-            highPriceOfTheDay = BigDecimal(110), lowPriceOfTheDay = BigDecimal(90),
-            openPriceOfTheDay = BigDecimal(100), previousClosePrice = BigDecimal(99),
-            timeStamp = LocalDateTime.now(), stockSymbol = stockSymbol
-        )
+        val bankAccount = createBankAccount().copy(balance = BigDecimal(500))
         val portfolioEntry = PortfolioEntry(
-            id = null, investmentAccountId = investmentAccountId, stockSymbol = stockSymbol,
-            quantity = calculatedVolume.toDouble(), totalInvestAmount = purchaseAmount
+            id = null,
+            investmentAccountId = investmentAccountId,
+            stockSymbol = stockSymbol,
+            quantity = calculatedVolume.toDouble(),
+            totalInvestAmount = purchaseAmount
         )
 
-        val latestQuote = StockQuoteLatest(
-            stockSymbol = "AAPL",
-            quoteId = 1L
-        )
+        val latestQuote = StockQuoteLatest(stockSymbol = stockSymbol, quoteId = 1L)
 
         `when`(investmentAccountRepository.findById(investmentAccountId)).thenReturn(Mono.just(investmentAccount))
         `when`(bankAccountRepository.findById(1L)).thenReturn(Mono.just(bankAccount))
-        doReturn(Mono.just(latestQuote))
-            .`when`(latestIStockQuoteRepository).findById(stockSymbol)
-
+        doReturn(Mono.just(latestQuote)).`when`(latestIStockQuoteRepository).findById(stockSymbol)
         `when`(quoteRepository.findById(1L)).thenReturn(Mono.just(stockQuote))
-        doReturn(Mono.empty<PortfolioEntry>())
-            .`when`(portfolioEntryRepository)
+        doReturn(Mono.empty<PortfolioEntry>()).`when`(portfolioEntryRepository)
             .findByInvestmentAccountIdAndStockSymbol(investmentAccountId, stockSymbol)
 
-
-        doReturn(Mono.just(portfolioEntry)).`when`(portfolioEntryRepository)
-            .save(any(PortfolioEntry::class.java))
-
-        doReturn(Mono.just(bankAccount.copy(balance = BigDecimal(300))))
-            .`when`(bankAccountRepository)
+        doReturn(Mono.just(portfolioEntry)).`when`(portfolioEntryRepository).save(any(PortfolioEntry::class.java))
+        doReturn(Mono.just(bankAccount.copy(balance = BigDecimal(300)))).`when`(bankAccountRepository)
             .save(any(BankAccount::class.java))
-
 
         val result = investmentAccountService.buyStock(investmentAccountId, stockSymbol, purchaseAmount).block()
 
@@ -175,11 +170,7 @@ class InvestmentAccountServiceTest {
         verify(investmentAccountRepository).findById(investmentAccountId)
         verify(bankAccountRepository).save(argThat { it.balance == BigDecimal(300) })
         verify(portfolioEntryRepository).save(argThat {
-            it.investmentAccountId == investmentAccountId &&
-                    it.stockSymbol == stockSymbol &&
-                    it.quantity == calculatedVolume.toDouble() &&
-                    it.totalInvestAmount == purchaseAmount
+            it.investmentAccountId == investmentAccountId && it.stockSymbol == stockSymbol && it.quantity == calculatedVolume.toDouble() && it.totalInvestAmount == purchaseAmount
         })
     }
-
 }
