273d9572ec5ae145cbe9affad8d99bc9ee9d9a91
David Thomann <david.thomann@student.hs-rm.de>
2025-01-24 01:54:22 +0100

    Added AssetOverview

---
+++ frontend/src/components/AssetOverview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="overview-flexbox">
+    <div cla>
+      <StockOverview></StockOverview>
+    </div>
+    <div>
+      <CryptoOverview></CryptoOverview>
+    </div>
+    <div>
+      <MetalOverview></MetalOverview>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import CryptoOverview from "@/components/CryptoOverview.vue";
+import MetalOverview from "@/components/MetalOverview.vue";
+import StockOverview from "@/components/StockOverview.vue";
+</script>
+
+<style scoped lang="scss">
+.overview-flexbox {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+
+}
+</style>
