3427e11b86d7ba26f12600207cf6739388c29dff
David Thomann <david.thomann@student.hs-rm.de>
2025-01-25 16:16:19 +0100

    Implemented missing DROP TABLE IF EXISTS for new Table favorite

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE If EXISTS investment_portfolio;
 DROP TABLE IF EXISTS portfolio_entry;
 DROP TABLE IF EXISTS stock_order;
+DROP TABLE IF EXISTS favorite;
 DROP TABLE IF EXISTS investmentaccount;
 DROP TABLE IF EXISTS exchange;
 DROP TABLE IF EXISTS bankaccount;
