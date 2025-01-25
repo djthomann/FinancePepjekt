a5577cd20a1079c8e51aeec16f091e541033b70d
Anna-Livia Martin <anna-livia.martin@student.hs-rm.de>
2025-01-25 18:47:29 +0100

    chore: delete unused imports and methods for repositories

--- src/main/kotlin/de/hsrm/mi/stacs/pepjekt/repositories/IPortfolioEntryRepository.kt
+++ src/main/kotlin/de/hsrm/mi/stacs/pepjekt/repositories/IPortfolioEntryRepository.kt
@@ -1,7 +1,6 @@
 package de.hsrm.mi.stacs.pepjekt.repositories
 
 import de.hsrm.mi.stacs.pepjekt.entities.PortfolioEntry
-import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
